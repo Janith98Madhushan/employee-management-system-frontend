3cc1db72944ddd4cb353c0117a58f819735f05f1
Janith Madhushan <janithmadhushan357@gmail.com>
2022-10-19 11:38:38 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+
+import { BrowserRouter as Router,Route,Switch } from 'react-router-dom';
+import './App.css';
+
+import FooterComponent from './component/FooterComponent';
+import HeaderComponent from './component/HeaderComponent';
+import ListEmployeeComponent from './component/ListEmployeeComponent';
+import AddEmployeeComponent from './component/AddEmployeeComponent';
+
+function App() {
+  return (
+    <div>
+      <Router>
+      
+        <HeaderComponent/>
+        <div className='container'>
+          <Switch>
+            <Route exact path="/" component = {ListEmployeeComponent}></Route>
+            <Route path="/employees" component = {ListEmployeeComponent}></Route>
+            <Route path="/add-employee" component={AddEmployeeComponent}></Route>
+            <Route path="/edit-employee/:id" component={AddEmployeeComponent}></Route>
+          </Switch>
+        </div>
+        
+        <FooterComponent/>
+
+      </Router>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/FooterComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const FooterComponent = () =>{
+    return(
+        <div>
+            <footer className="footer">
+                <span className="text-muted">All Right Reserved 2022 @Janith.com</span>
+            </footer>
+        </div>
+    )
+}
+
+export default FooterComponent;
